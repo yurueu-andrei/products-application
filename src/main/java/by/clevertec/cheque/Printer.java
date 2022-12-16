ef8e0fbd3969163a3906e674b0f5a0cfc7f079b9
Dobrowydka <andrikchannelyou@gmail.com>
2022-12-16 18:25:42 +0300

    better output

--- src/main/java/by/clevertec/cheque/Printer.java
+++ src/main/java/by/clevertec/cheque/Printer.java
@@ -6,14 +6,14 @@ import java.time.temporal.ChronoUnit;
 
 public class Printer {
     public void printHeader() {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("\t\t\t\t\t\tCASH RECEIPT\t\t\t\t\t\t");
+        System.out.println("\t\t\t\t\tCASH RECEIPT");
+        System.out.println("----------------------------------------------------------------------------------------");
         System.out.println("\tSUPERMARKET 123\t\t\t\t\t\t\t\t\t\t\t");
         System.out.println("\ti2, MILKYWAY Galaxy/Earth\t\t\t\t\t\t\t\t");
         System.out.println("\tTEL: +375-25-999-99-99\t\t\t\t\t\t\t\t\t");
         System.out.println("\tCASHIER: Andrei Yurueu\t\t\t\tDATE: " + LocalDate.now() + "\t");
-        System.out.println("\t\t\t\t\t\t\t\t\t\tTIME: " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "\t\t");
-        System.out.println("----------------------------------------------------------");
+        System.out.println("\t\t\t\t\t\t\tTIME: " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "\t\t");
+        System.out.println("----------------------------------------------------------------------------------------");
         System.out.println("\tQTY\t\tDESCRIPTION\t\t\t\tPRICE\t\tTOTAL\t\t");
     }
 
@@ -26,11 +26,11 @@ public class Printer {
     }
 
     public void printFooter(float total, float discount) {
-        System.out.println("==========================================================");
-        System.out.println("\tTOTAL:\t\t\t\t\t\t\t\t\t\t" + String.format("%.2f$", total));
-        System.out.println("\tDISCOUNT:\t\t\t\t\t\t\t\t\t" + String.format("%.2f$", discount));
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("\tTOTAL:\t\t\t\t\t\t" + String.format("%.2f$", total));
+        System.out.println("\tDISCOUNT:\t\t\t\t\t" + String.format("%.2f$", discount));
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-        System.out.println("\tTOTAL WITH DISCOUNT:\t\t\t\t\t\t" + String.format("%.2f$", total - discount));
-        System.out.println("----------------------------------------------------------");
+        System.out.println("\tTOTAL WITH DISCOUNT:\t\t\t\t" + String.format("%.2f$", total - discount));
+        System.out.println("----------------------------------------------------------------------------------------");
     }
 }
