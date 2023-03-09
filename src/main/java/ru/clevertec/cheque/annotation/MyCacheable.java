3b831531480785e4b3a453bec5b85cb4e707f1c4
Dobrowydka <andrikchannelyou@gmail.com>
2023-03-09 16:55:31 +0300

    feat: add my own cacheable annotation

---
+++ src/main/java/ru/clevertec/cheque/annotation/MyCacheable.java
@@ -0,0 +1,8 @@
+package ru.clevertec.cheque.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyCacheable {
+}
