ab0f806118764bd1b8312ceb31435b3ddb944d11
Dobrowydka <andrikchannelyou@gmail.com>
2023-03-09 16:41:26 +0300

    docs: javadoc for cardMapper

---
+++ src/main/java/ru/clevertec/cheque/mapper/CardMapper.java
@@ -0,0 +1,19 @@
+package ru.clevertec.cheque.mapper;
+
+import ru.clevertec.cheque.dto.CardDto;
+import ru.clevertec.cheque.dto.CardSaveDto;
+import ru.clevertec.cheque.model.entity.DiscountCard;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+/**
+ * Mapper class for cards. Used to convert <b>Entities</b> into <b>DTO</b> operations.
+ * @author Yuryeu Andrei
+ */
+@Mapper(componentModel = "spring")
+public interface CardMapper {
+    CardDto toDto(DiscountCard card);
+    DiscountCard fromSaveDto(CardSaveDto cardSaveDto);
+    List<CardDto> toDto(List<DiscountCard> cards);
+}
