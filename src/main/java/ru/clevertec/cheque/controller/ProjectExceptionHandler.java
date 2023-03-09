10aac54020347c5530ed54c78e97f7efaefb1d3a
Dobrowydka <andrikchannelyou@gmail.com>
2023-03-09 16:54:50 +0300

    feat: add exception handler for MethodArgumentNotValidException

--- src/main/java/ru/clevertec/cheque/controller/ProjectExceptionHandler.java
+++ src/main/java/ru/clevertec/cheque/controller/ProjectExceptionHandler.java
@@ -1,16 +1,15 @@
-package by.clevertec.cheque.controller;
+package ru.clevertec.cheque.controller;
 
+import ru.clevertec.cheque.exception.ServiceException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -21,7 +20,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @ControllerAdvice
-public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
+public class ProjectExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -31,6 +30,22 @@ public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
     ) {
         log.error("handleServiceExceptionServerError {}\n", request.getRequestURI(), ex);
         return new ApiCallError(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiCallDetailedError<?> handleMethodArgumentNotValid(
+            HttpServletRequest request,
+            MethodArgumentNotValidException ex
+    ) {
+        List<Map<String, String>> details = new ArrayList<>();
+        Map<String, String> detail = new HashMap<>();
+        detail.put("field", ex.getBindingResult().getFieldError().getField());
+        detail.put("value", ex.getBindingResult().getFieldError().getRejectedValue().toString());
+        detail.put("info", ex.getFieldError().getDefaultMessage());
+        details.add(detail);
+        log.error("handleServiceExceptionServerError {}\n", request.getRequestURI(), ex);
+        return new ApiCallDetailedError<>("Validation failed", details);
     }
 
     @ExceptionHandler(ServiceException.class)
@@ -44,11 +59,10 @@ public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
         Map<String, String> detail = new HashMap<>();
         detail.put("info", ex.getMessage());
         details.add(detail);
-        return new ApiCallDetailedError<>("Parameters is not valid", details);
+        return new ApiCallDetailedError<>("Parameters are not valid", details);
     }
 
     @Data
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class ApiCallDetailedError<T> {
         private String message;
@@ -56,7 +70,6 @@ public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Data
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class ApiCallError {
         private String message;
